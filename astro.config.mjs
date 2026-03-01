82bf5ab8210fe31fd8c42a15c273fc379467a29f
babosuho <admin@makorang.com>
2026-03-01 23:10:37 +0900

    site URL 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://my-blog-pages-a94.pages.dev',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
